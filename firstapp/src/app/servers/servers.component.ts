5c3f8799181935a9ee109a6371f68a6282774d73
¨Juan_Aleman¨ <jmayorga92@hotmail.com>
2022-12-11 17:22:30 -0600

    Practica de los videos del curso de angular: * Creación de componentes en angular * Implementación de estilos en angular

---
+++ firstapp/src/app/servers/servers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+   selector: 'app-servers',
+ // selector: '[app-servers]', // selector  forma #2
+ // selector: '.app-servers', // selector  forma #3
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent {
+
+}
