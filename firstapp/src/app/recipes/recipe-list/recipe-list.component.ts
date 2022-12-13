7daa5fe25b00e1c7d16800aad5a93186076e43d2
¨Juan_Aleman¨ <jmayorga92@hotmail.com>
2022-12-13 15:00:10 -0600

    Ajustando los componentes de recetas

--- firstapp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ firstapp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simplye a test', 'https://static.onecms.io/wp-content/uploads/sites/43/2022/02/16/21014-Good-old-Fashioned-Pancakes-mfs_001.jpg')
+ 
+  ];
 
+  constructor(){ }
+
+  ngOnInit(): void {
+    
+  }
 }
