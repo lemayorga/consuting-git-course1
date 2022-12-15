0316a94c95750c23422649a95c6b6cd92a571a3f
¨Juan_Aleman¨ <jmayorga92@hotmail.com>
2022-12-14 23:12:04 -0600

    Bindiando shopping-list

--- firstapp/src/app/shopping-list/shoppint-edit/shoppint-edit.component.ts
+++ firstapp/src/app/shopping-list/shoppint-edit/shoppint-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { outputAst } from '@angular/compiler';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shoppint-edit',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./shoppint-edit.component.css']
 })
 export class ShoppintEditComponent {
+ 
+ @ViewChild('nameInput') nameInputRef : ElementRef;
+ @ViewChild('amountInput') amountInputRef : ElementRef;
+ @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
+  onAddItem(){
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
