c94915ac7102a207ac2844d713c3ea5a1061945d
¨Juan_Aleman¨ <jmayorga92@hotmail.com>
2022-12-14 18:26:24 -0600

    Agregando la funcionalidad de tab menu

--- firstapp/src/app/app.component.ts
+++ firstapp/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firstapp';
+  loadedFeacture: string = 'recipe';
+
+  onNavigate(feacture:string){
+    this.loadedFeacture = feacture;
+  }
 }
